b00845d186312f095360c22e9f5ecb39abad4c4e
PsammystPrime <127736051+PsammystPrime@users.noreply.github.com>
2023-12-08 22:38:36 +0300

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,26 +1,27 @@
-//create constructor for novels
-function Novels(title, author, pages, status,) {
+const myLibrary = [];
+//create constructor for Book
+function Book(title, author, pages, status,) {
     this.title = title,
     this.author = author,
     this.pages = pages,
     this.status = status
   };
   //create method for calling constructor
-    Novels.prototype.info = function () {
+    Book.prototype.info = function () {
       return this.title +' by '+this.author+ ' has '+this.pages+ ' pages, '+this.status
     };
-    //create novels
-  const novel1=new Novels ('War', 'warrior',50, 'read') ;
-  const novel2=new Novels('Engineering Maths', 'J Bird', 500, 'Unread');
-  const novel3=new Novels('Think And Grow Rich', 'Napoleon hill', 200, 'read');
-  const novel4=new Novels('Romeo & Juliet', 'Shakespear', '350', 'Unread' );
-  const novel5=new Novels('Rich Dad Poor Dad', 'R Kowarski', 400, 'Unread');
-  const novel6=new Novels('True Masculinity', 'Unknown', 300, 'Unread');
-  const novel7=new Novels('The Last Ship', 'Tom Chandler',900 , 'Unread');
-  const novel8=Object.create(Novels);
-  novel8.title = 'hunger'    
-  console.log(novel1.info());
-  console.log(novel2.info());
-  console.log(novel3.info());
-  console.log(novel8.info());
+    //create Book
+  const book1=new Book ('War', 'warrior',50, 'read') ;
+  const book2=new Book('Engineering Maths', 'J Bird', 500, 'Unread');
+  const book3=new Book('Think And Grow Rich', 'Napoleon hill', 200, 'read');
+  const book4=new Book('Romeo & Juliet', 'Shakespear', '350', 'Unread' );
+  const book5=new Book('Rich Dad Poor Dad', 'R Kowarski', 400, 'Unread');
+  const book6=new Book('True Masculinity', 'Unknown', 300, 'Unread');
+  const book7=new Book('The Last Ship', 'Tom Chandler',900 , 'Unread');
+  const book8=Object.create(Book);
+  book8.title = 'hunger'    
+  console.log(book1.info());
+  console.log(book2.info());
+  console.log(book3.info());
+  console.log(book8.info());
   
