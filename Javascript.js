1b5dad2a9e2611b99ad0c1e5c16c9db3e6df1770
PsammystPrime <127736051+PsammystPrime@users.noreply.github.com>
2023-12-11 04:28:40 +0300

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -58,16 +58,3 @@ function Book(title, author, pages, status,) {
       addBookToLibrary()
 
 
-
-
-    //create Book
- /* const book1=new Book ('War', 'warrior',50, 'read') ;
-  const book2=new Book('Engineering Maths', 'J Bird', 500, 'Unread');
-  const book3=new Book('Think And Grow Rich', 'Napoleon hill', 200, 'read');
-  const book4=new Book('Romeo & Juliet', 'Shakespear', '350', 'Unread' );
-  const book5=new Book('Rich Dad Poor Dad', 'R Kowarski', 400, 'Unread');
-  const book6=new Book('True Masculinity', 'Unknown', 300, 'Unread');
-  const book7=new Book('The Last Ship', 'Tom Chandler',900 , 'Unread'); 
-  console.log(book1.info());
-  console.log(book2.info());
-  console.log(book3.info());*/
