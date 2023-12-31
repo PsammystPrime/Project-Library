c5d62ab37ee6b4ab9ac25fae20458f7be28ca0eb
PsammystPrime <samuelprime384@gmail.com>
2023-12-31 15:54:04 +0300

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const Libraries = [];
 
 function Book(title, author, pages, status,) {
     this.title = title,
@@ -10,6 +10,8 @@ function Book(title, author, pages, status,) {
     Book.prototype.info = function () {
       return this.title +' by '+this.author+ ' has '+this.pages+ ' pages, '+this.status
     };
+
+
 
     function addBookToLibrary() {  
         const title = document.querySelector('#title');
@@ -24,13 +26,14 @@ function Book(title, author, pages, status,) {
           e.preventDefault()
           if (title.value && author.value && pages.value!==''  ) {
             novel=new Book(title.value,author.value,pages.value)
-            myLibrary.push(novel)
+            Libraries.push(novel)
 
           notification.textContent = 'Book added Successfully!'
           const list1 =document.createElement('li')
           const info = document.createElement('span')
           const check = document.createElement('span')
           const del = document.createElement('button')
+          
 
           info.textContent= title.value+' by '+author.value+ ' has '+pages.value+ ' pages.'          
             if (status.checked) {
@@ -61,9 +64,27 @@ function Book(title, author, pages, status,) {
 
         });       
 
+
+   
       }
       addBookToLibrary()
 
+      /**************************************Not Working */
 
+      const search = document.querySelector('[data-search]')
+      search.addEventListener('input', (e)=>{
+         const value = e.target.value
+         Libraries.forEach(library => {
+         const isVisible = library.title.includes(value);
+         
+          const element = document.querySelectorAll('li');
+          element.classList.toggle('hide', !isVisible)
+         
+        
+         //element.classList.toggle("hide", !isVisible)
+       //  console.log(isVisible)
+         })
+        
+      })
 
 
