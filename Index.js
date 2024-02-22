d073b7ac5cfbac6dddd8f0628553291d89295b6c
PsammystPrime <samuelprime384@gmail.com>
2024-02-22 13:47:33 +0300

    Pseudo code: create objects and push them to array

--- Index.js
+++ Index.js
@@ -7,8 +7,19 @@ function Book(title, author, pages, status) {
       return console.log(this.title+' by '+this.author+' with '+this.pages+' pages ')
     }
   }
-  const book1 = new Book('The 100', 'Kass Morgan','200', 'read')
-  console.log(book1.info());
+  const book1 = new Book('The 100', 'Kass Morgan','100', 'read')
+  const book2 = new Book('The 100', 'Kass Morgan','200', 'read')
+  const book3 = new Book('The 200', 'Kent Morgan','300', 'unread')
+//   console.log(book1.info());
+//   console.log(book2.info());
+//   console.log(book3.info());
+//tired of consoling everything manually
+const library = [];
+library.push(book1)
+library.push(book2)
+library.push(book3)
+console.log(library);
+
   /*1: Instead of hard coding book details, we use a form that fetches it.
   2: Instaed of calling the book, we add an event listener for submit button
   that calls book info().
